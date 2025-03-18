612933b97691c09c89852fe551a25512460d1287
minot michael <nexspro@ik.me>
2025-03-18 16:26:21 +0100

    upgrade model + routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,25 @@
 Rails.application.routes.draw do
-  get 'steps/completed'
-  get 'careers/show'
-  get 'careers/create'
-  get 'favorites/create'
-  get 'psy_profiles/show'
-  get 'jobs/show'
-  get 'answers/new'
-  get 'answers/create'
   devise_for :users
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  root to: "pages#home"
+
+  get "/dashboard", to: "pages#dashboard"
+  resources :psy_profiles, only: [:show]
+  resources :jobs, only: [:show] do
+    resources :favorites, only: [:create]
+  end
+  resources :answers, only: [:new, :create]
+  resources :careers, only: [:show, :create]
+  resources :steps, only: [] do
+    member do
+      patch :completed
+    end
+  end
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
+  #get "up" => "rails/health#show", as: :rails_health_check
   # Defines the root path route ("/")
   # root "posts#index"
+  #
 end
