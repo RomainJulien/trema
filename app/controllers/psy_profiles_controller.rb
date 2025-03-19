0a3d92ed678364fe1039e0101f0f2e857041660b
minot michael <nexspro@ik.me>
2025-03-19 18:11:24 +0100

    design psy

--- app/controllers/psy_profiles_controller.rb
+++ app/controllers/psy_profiles_controller.rb
@@ -1,4 +1,8 @@
 class PsyProfilesController < ApplicationController
   def show
+    @psy_profile = PsyProfile.find_by(id: params[:id])
+    if @psy_profile.nil?
+      redirect_to root_path, alert: "Profil introuvable"
+    end
   end
 end
