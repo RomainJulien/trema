9a65411768501139ae009406d374b9622fdcc5ea
Mikael Renvoise <klimae@icloud.com>
2025-03-20 17:28:50 +0100

    lien psyprofile ok

--- app/controllers/psy_profiles_controller.rb
+++ app/controllers/psy_profiles_controller.rb
@@ -1,8 +1,11 @@
 class PsyProfilesController < ApplicationController
   def show
     @psy_profile = PsyProfile.find_by(id: params[:id])
+
     if @psy_profile.nil?
       redirect_to root_path, alert: "Profil introuvable"
     end
+
   end
+
 end
