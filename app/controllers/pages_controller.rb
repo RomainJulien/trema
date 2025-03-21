e427a3c5fe05f33c181cfad271d8b0db804f45e1
minot michael <nexspro@ik.me>
2025-03-21 15:11:40 +0100

    bouton dash

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def dashboard
+    @psy_profile = current_user.psy_profile
     @job_psy = [
       { title: "Ébéniste", image_url: "ebeniste.png" },
       { title: "Cuisinier", image_url: "cuisinier.png" },
