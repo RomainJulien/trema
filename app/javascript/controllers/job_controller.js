3bcf5fd6c54656380196bb3df4f7cbf7520d9982
romainjulien <romjulien.dev@gmail.com>
2025-03-27 19:48:19 +0100

    focking shit

--- app/javascript/controllers/job_controller.js
+++ app/javascript/controllers/job_controller.js
@@ -2,33 +2,28 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="job"
 export default class extends Controller {
-  static targets = ["careerTitle"];
-  connect() {
-    console.log("Le controller est connecté!", this.element);
-  }
-  selectJob() {
-    console.log("Sélection du métier déclenchée !");
-    console.log("Cible trouvée :", this.careerTitleTarget); // Log de la cible
+  static targets = ["careerTitle"]
 
-    event.target.disabled = true; // Désactive le bouton
+  connect() {
+    console.log("Le controller est connecté!", this.element)
+  }
+
+  selectJob(event) {
+    console.log("Sélection du métier déclenchée !")
+    console.log("Cible trouvée :", this.careerTitleTarget)
+
+    event.target.disabled = true
 
     if (!document.getElementById("selected-job")) {
-      let newTextDiv = document.querySelector(".dashboard-career-header");
+      const header = document.querySelector(".dashboard-career-header")
+      const imagePath = header.dataset.image
 
-      newTextDiv.insertAdjacentHTML("beforeend", '<img src="/assets/psy_profiles_logo/ebeniste.png" class="dashboard-profile-logo"><span class="dashboard-profile-highlight"> Ébéniste </span>');
-      /* newText.style.fontSize = "2em";
-      newText.style.fontsize= 40px;// Taille exacte depuis Figma
-      newText.style.fontWeight = "bold";
-      newText.style.marginTop = "10px"; */
+      const newHTML = `
+        <img src="${imagePath}" class="dashboard-profile-logo">
+        <span class="dashboard-profile-highlight" id="selected-job">Ébéniste</span>
+      `
 
-      /* newText.style.fontfamily: 'Chillax', sans-serif; // Police utilisée
-      /* newText.style.fontweight: 600;                   // Semibold
-      newText.style.lineheight: 140%;
-      newText.style.letterspacing: 0.2px;
-      newText.style.color:  #DA4167; */
-
-
-
+      header.insertAdjacentHTML("beforeend", newHTML)
     }
   }
 }
